375dcde31c8ad512e931bb7c533a5dcc5e0fcc71
Zaknefeinn <zaknefeinn@gmail.com>
2018-07-06 15:52:51 -0500

    github pages modifications

--- src/components/utils/getSchedule.js
+++ src/components/utils/getSchedule.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 export const getSchedule = (cmd, orig, dest, date, time) => {
   return axios.get(
-    `http://api.bart.gov/api/sched.aspx?cmd=${cmd}&key=MW9S-E7SL-26DU-VV8V&orig=${orig}&dest=${dest}&time=${time}&date=${date}&b=2&a=4&json=y`
+    `https://api.bart.gov/api/sched.aspx?cmd=${cmd}&key=MW9S-E7SL-26DU-VV8V&orig=${orig}&dest=${dest}&time=${time}&date=${date}&b=2&a=4&json=y`
   );
 };
 
 export const getStations = () => {
   return axios.get(
-    'http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y'
+    'https://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y'
   );
 };
